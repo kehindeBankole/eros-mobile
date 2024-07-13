40d0de3d888e847d069c1fa37bb89d9c6ed714b6
kehindeBankole <bankolek1@gmail.com>
2024-07-13 14:21:10 +0100

    Initial Commit

---
+++ ErosMobile/ErosMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErosMobileApp.swift
+//  ErosMobile
+//
+//  Created by Kehinde Bankole on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ErosMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
